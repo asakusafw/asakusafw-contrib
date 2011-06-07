3a6197dde08ed5932b28cb6673f44d86cf2a248e
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 15:02:12 +0900

    modify reference to other script.

--- quick-start/cdh3vm/bin/switch_to_pseudo.sh
+++ quick-start/cdh3vm/bin/switch_to_pseudo.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
 
 sudo update-alternatives --set hadoop-0.20-conf /etc/hadoop-0.20/conf.pseudo
-./restart_hadoop_processes.sh
+"$bin"/restart_hadoop_processes.sh
 ruby -i -pe '$_.sub!("hdfs-protocol-host=file:///", "hdfs-protocol-host=hdfs://localhost:8020")' $ASAKUSA_HOME/bulkloader/conf/bulkloader-conf-hc.properties
 
--- quick-start/cdh3vm/bin/switch_to_standalone.sh
+++ quick-start/cdh3vm/bin/switch_to_standalone.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-./stop_hadoop_processes.sh
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+"$bin"/stop_hadoop_processes.sh
 sudo update-alternatives --set hadoop-0.20-conf /etc/hadoop-0.20/conf.empty
 ruby -i -pe '$_.sub!("hdfs-protocol-host=hdfs://localhost:8020", "hdfs-protocol-host=file:///")' $ASAKUSA_HOME/bulkloader/conf/bulkloader-conf-hc.properties
 
