0933806d6c3d2dd5db35c238b712ff421d31dbe2
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 23:14:09 +0900

    show databases and users on mysql instration.

--- quick-start/cdh3vm/scripts/setup_mysql.sh
+++ quick-start/cdh3vm/scripts/setup_mysql.sh
@@ -3,7 +3,7 @@
 # MySQLのユーザーの作成
 echo "
 ==================================================================================================
-setup mysql, create asakusa user
+setup mysql, create user asakusa and database asakusa. 
 ==================================================================================================
 "
 sleep 1
@@ -13,3 +13,15 @@ GRANT ALL PRIVILEGES ON *.* TO 'asakusa'@'%'IDENTIFIED BY 'asakusa' WITH GRANT O
 DROP DATABASE IF EXISTS asakusa;
 CREATE DATABASE asakusa DEFAULT CHARACTER SET utf8;
 __EOF__
+
+mysql -u root -proot << __EOF__
+show databases;
+__EOF__
+
+echo ""
+
+mysql mysql -u root -proot << __EOF__
+select host,user from user;
+__EOF__
+
+
