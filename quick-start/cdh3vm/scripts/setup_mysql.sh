0b3d32fabdc23c1c313c8e75fc349b9f4014163d
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 18:15:36 +0900

    modify typo

--- quick-start/cdh3vm/scripts/setup_mysql.sh
+++ quick-start/cdh3vm/scripts/setup_mysql.sh
@@ -6,4 +6,9 @@ setup mysql, create asakusa user
 ==================================================================================================
 "
 sleep 1
-mysql -u root -p < setup_mysql.sql
+mysql -u root -p << __EOF__
+GRANT ALL PRIVILEGES ON *.* TO 'asakusa'@'localhost' IDENTIFIED BY 'asakusa' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'asakusa'@'%'IDENTIFIED BY 'asakusa' WITH GRANT OPTION;
+DROP DATABASE IF EXISTS asakusa;
+CREATE DATABASE asakusa DEFAULT CHARACTER SET utf8;
+__EOF__
