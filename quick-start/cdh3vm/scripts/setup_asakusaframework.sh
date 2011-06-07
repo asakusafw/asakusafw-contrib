936df60b69f6fa3194ca7d9ad565373bf36ad119
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 12:26:36 +0900

    add instraction for setup asakusaframework.

---
+++ quick-start/cdh3vm/scripts/setup_asakusaframework.sh
@@ -0,0 +1,8 @@
+# Asakuasのセットアップ
+mkdir ~/workspace
+cd ~/workspace
+
+git clone git://github.com/asakusafw/asakusafw-examples.git
+cd ./asakusafw-examples/example-tutorial
+mvn clean assembly:single antrun:run
+
--- quick-start/cdh3vm/setup.sh
+++ quick-start/cdh3vm/setup.sh
@@ -7,6 +7,8 @@ sudo ./scripts/install_maven.sh
 sudo ./scripts/configure_apparmor.sh
 ./scripts/configure_ssh.sh
 ./scripts/congifure_bashrc.sh
+./scripts/setup_asakusaframework.sh
+./bin/switch_to_pseudo.sh
 source ~/.bashrc
 
 cat ./messages/end.txt
