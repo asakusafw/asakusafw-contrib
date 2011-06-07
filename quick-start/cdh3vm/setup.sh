8117b20ac1eb0a0de5e6c62d2235b49cfa7837e9
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 12:07:02 +0900

    add instration messages

--- quick-start/cdh3vm/setup.sh
+++ quick-start/cdh3vm/setup.sh
@@ -1,7 +1,12 @@
+cat ./messages/start.txt
+
 sudo ./scripts/install_mysql.sh
 sudo ./scripts/install_git.sh
+sudo ./scripts/install_ruby.sh
 sudo ./scripts/install_maven.sh
 sudo ./scripts/configure_apparmor.sh
 ./scripts/configure_ssh.sh
 ./scripts/congifure_bashrc.sh
 source ~/.bashrc
+
+cat ./messages/end.txt
