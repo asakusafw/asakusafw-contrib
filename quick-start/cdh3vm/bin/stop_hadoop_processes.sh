bd0eb7022787c73bba992065f10bb52706da95d6
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 13:53:43 +0900

    modify hadoop utilities

---
+++ quick-start/cdh3vm/bin/stop_hadoop_processes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for service in /etc/init.d/hadoop-0.20-*
+do
+ sudo $service stop
+done
+
--- quick-start/cdh3vm/bin/switch_to_standalone.sh
+++ quick-start/cdh3vm/bin/switch_to_standalone.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo /root/bin/stop-all.sh
-sudo /usr/sbin/alternatives --set hadoop-0.20-conf /etc/hadoop-0.20/conf.stand_alone
+./stop_hadoop_processes.sh
+sudo update-alternatives --set hadoop-0.20-conf /etc/hadoop-0.20/conf.empty
 ruby -i -pe '$_.sub!("hdfs-protocol-host=hdfs://localhost:8020", "hdfs-protocol-host=file:///")' $ASAKUSA_HOME/bulkloader/conf/bulkloader-conf-hc.properties
 
