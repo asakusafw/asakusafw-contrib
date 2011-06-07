744758ad664a425be620b86f707ffbba5b2df4c1
shingo.furuyama <shingo.furuyama@nautilus-technologies.com>
2011-06-07 13:56:52 +0900

    modify instration order.

--- quick-start/cdh3vm/setup.sh
+++ quick-start/cdh3vm/setup.sh
@@ -7,8 +7,8 @@ sudo ./scripts/install_maven.sh
 sudo ./scripts/configure_apparmor.sh
 ./scripts/configure_ssh.sh
 ./scripts/congifure_bashrc.sh
+source ~/.bashrc
 ./scripts/setup_asakusaframework.sh
 ./bin/switch_to_pseudo.sh
-source ~/.bashrc
 
 cat ./messages/end.txt
